f34d14f3471d56b4031a7fa4e8f31beb7c383dfc
Natbud <96924057+Natbud@users.noreply.github.com>
2023-12-06 06:11:51 +0000

    Update 01_02.py

--- 01_02.py
+++ 01_02.py
@@ -8,19 +8,36 @@ first_digits = []
 last_digits = []
 final_digits = []
 
-text_digit_set = {'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'}
+text_digit_list = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+#print("text_digit_set:", text_digit_list)
 
 text_locations = []
 
 
 #Get location of all/any text-digits in line:
 for i, line in enumerate(file_list):
+    ordered_digits = [] # don't do this:   set =  {}   that creates a dictionary!
     #print("LINE: ", i+1)
-    for text_digit in text_digit_set:
+    for p, text_digit in enumerate(text_digit_list):
         text_digit_location = line.find(text_digit)
         if text_digit_location >-1:
-            print("line: ", i+1, "digit:", text_digit, "location: ", text_digit_location)
+            #print("line: ", i+1, "text_digit:", text_digit, "location: ", text_digit_location)
+            digit_with_location = (text_digit_location,p+1)
+            ordered_digits.append(digit_with_location)
+            #ordered_digits_set.update([text_digit_location,p+1])
+            sorted_digits = sorted(ordered_digits)
+
+    print("Line: ",i+1, "orderd_digits: ", sorted_digits)
+
+
+"""
+
             #store text_digit and text_digit_location pairs (dictionary?   List of lists?)
+    for char in line:
+        if char.isdigit():
+            digit_location = line.find(char)
+            print("line: ",i+1, "digit:",char, "location: ", digit_location )
+
 
 
 
@@ -44,7 +61,7 @@ for r, line in enumerate(file_list):
 
 
 
-"""
+
 #Get last digits:
 for line in file_list:
     for char in reversed(line):
