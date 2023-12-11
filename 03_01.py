c2dc5e3cbca3cd8fa9fb1b462d74e1b3447183fb
Natbud <96924057+Natbud@users.noreply.github.com>
2023-12-11 07:07:20 +0000

    03_01

---
+++ 03_01.py
@@ -0,0 +1,5 @@
+thefilepath = "03_01_Data_Test.txt"
+
+with open(thefilepath) as f:
+    file_list = f.readlines()
+    file_list = [line.strip() for line in file_list]   #strips out the /n character from each line
